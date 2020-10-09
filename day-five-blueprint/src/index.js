a95804d1a6fe0e47fc82bd8826c2fc48cf5d37c4
heyhiheyhi <58721040+heyhiheyhi@users.noreply.github.com>
2020-10-10 01:27:54 +0900

    something do

--- day-five-blueprint/src/index.js
+++ day-five-blueprint/src/index.js
@@ -3,10 +3,18 @@ import "./styles.css";
 // You're gonna need this
 const NINE_HOURS_MILLISECONDS = 32400000;
 
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h4");
+
 function getTime() {
   // Don't delete this.
-  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  //const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  const xmasDay = new Date();
+  const date = xmasDay.getDate;
+  clockTitle.innerText = `${date}`;
 }
 
-function init() {}
+function init() {
+  getTime();
+}
 init();
