b87d865570e264189516ddaa2509cb08a2ccc352
heyhiheyhi <58721040+heyhiheyhi@users.noreply.github.com>
2020-10-11 01:38:32 +0900

    Make it challenge
    Study for store to local strorage even refresh

--- day-six-blueprint/src/index.js
+++ day-six-blueprint/src/index.js
@@ -1,3 +1,22 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+//import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const select = document.querySelector("select");
+const option = select.querySelectorAll("option");
+
+function saveValue(v) {
+  localStorage.setItem("country", v);
+}
+
+function selectCountry(event) {
+  const currentValue = event.target.value;
+  saveValue(currentValue);
+  console.log(currentValue);
+}
+
+function init() {
+  select.addEventListener("change", selectCountry);
+}
+
+init();
